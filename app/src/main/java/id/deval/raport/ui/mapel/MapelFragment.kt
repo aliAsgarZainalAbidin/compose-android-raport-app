18c8dc02028af0633097fefd4f51e87beddf5a57
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2022-08-03 07:54:12 +0800

    layouting MapelFragment.kt

--- app/src/main/java/id/deval/raport/ui/mapel/MapelFragment.kt
+++ app/src/main/java/id/deval/raport/ui/mapel/MapelFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.deval.raport.R
 import id.deval.raport.databinding.FragmentMapelBinding
+import id.deval.raport.db.models.Mapel
+import id.deval.raport.ui.RvAdapter
+import id.deval.raport.utils.DummyData
+import id.deval.raport.utils.OperationsTypeRv
+import id.deval.raport.utils.hide
+import id.deval.raport.utils.show
 
 class MapelFragment : Fragment() {
 
@@ -19,5 +26,26 @@ class MapelFragment : Fragment() {
     ): View? {
         _binding = FragmentMapelBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataMapel = DummyData().setDummyDataMapel()
+        with(binding){
+            mtvMapelName.text = "Admin"
+            mtvMapelTotalmapel.text = dataMapel.size.toString()
+            includeRvMapel.mtvRvlayoutAdd.show()
+            includeRvMapel.mtvRvlayoutAdd.text = "Tambah Mapel"
+            includeRvMapel.mtvRvlayoutTitle.text = "Mata Pelajaran"
+            includeRvMapel.mtvRvlayoutViewmore.hide()
+            includeRvMapel.rvRvlayoutContainer.apply {
+                val adapter = RvAdapter<Mapel>("mapel",OperationsTypeRv.READ)
+                adapter.setData(dataMapel)
+                adapter.notifyDataSetChanged()
+                this.adapter = adapter
+                layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            }
+        }
     }
 }
