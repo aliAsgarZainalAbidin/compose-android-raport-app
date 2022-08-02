5999e5fd24d24cdb4abe31642f8702744c1bc856
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2022-08-02 15:07:43 +0800

    layouting RegistrasiSiswaFragment.kt

---
+++ app/src/main/java/id/deval/raport/ui/akun/siswa/RegistrasiSiswaFragment.kt
@@ -0,0 +1,54 @@
+package id.deval.raport.ui.akun.siswa
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.deval.raport.R
+import id.deval.raport.databinding.FragmentRegistrasiSiswaBinding
+import id.deval.raport.db.models.Siswa
+import id.deval.raport.ui.RvAdapter
+import id.deval.raport.utils.DummyData
+import id.deval.raport.utils.HelperView
+import id.deval.raport.utils.OperationsTypeRv
+
+class RegistrasiSiswaFragment : Fragment() {
+
+    private lateinit var _binding : FragmentRegistrasiSiswaBinding
+    private lateinit var navController: NavController
+    private lateinit var dataSiswa : ArrayList<Siswa>
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegistrasiSiswaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = HelperView.getMainNavController(requireActivity())
+        dataSiswa = DummyData().setDummyDataSiswa()
+        with(binding){
+            includeRvGuru.mtvRvlayoutTitle.text = "Siswa"
+            includeRvGuru.rvRvlayoutContainer.apply {
+                val adapter = RvAdapter<Siswa>("siswa", OperationsTypeRv.EDIT)
+                adapter.setData(dataSiswa)
+                adapter.notifyDataSetChanged()
+                this.adapter = adapter
+                layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            }
+
+            mbRegistrasisiswaAdd.setOnClickListener {
+
+            }
+        }
+    }
+
+}
