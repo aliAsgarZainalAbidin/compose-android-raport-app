c556adad2c1143d90451b5134b0328c45277da76
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2022-08-09 22:47:09 +0800

    implementasi AddSiswa addOrangtua (NOT TESTED YET)

--- app/src/main/java/id/deval/raport/ui/akun/siswa/AddOrangTuaFragment.kt
+++ app/src/main/java/id/deval/raport/ui/akun/siswa/AddOrangTuaFragment.kt
@@ -8,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import id.deval.raport.R
 import id.deval.raport.databinding.FragmentAddOrangTuaBinding
+import id.deval.raport.db.models.Account
 import id.deval.raport.db.models.Siswa
 import id.deval.raport.utils.BaseSkeletonFragment
 import id.deval.raport.utils.Constanta
+import id.deval.raport.utils.showToast
 
 class AddOrangTuaFragment : BaseSkeletonFragment() {
 
@@ -28,16 +30,58 @@ class AddOrangTuaFragment : BaseSkeletonFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val siswa = arguments?.getParcelable(Constanta.PARCELABLE_ITEM) ?: Siswa()
+        val siswa: Siswa? = arguments?.getParcelable(Constanta.PARCELABLE_ITEM)
+        var alamat = ""
 
         Log.d("TAG", "onViewCreated: $siswa")
         with(binding) {
+            if (siswa != null) {
+                tietAddorangtuaNisn.setText(siswa.nis)
+                alamat =
+                    if (siswa.address != null && siswa.address != "-") siswa.address else siswa.alamatWali.toString()
+                tietAddorangtuaAlamat.setText(alamat)
+                tietAddorangtuaHp.setText(siswa.phone)
+            }
 
             ivAddorangtuaBack.setOnClickListener {
                 mainNavController.popBackStack()
             }
             mbAddorangtuaSimpan.setOnClickListener {
-                mainNavController.popBackStack(R.id.addSiswaFragment, true)
+                val namaLengkap = tietAddorangtuaNamalengkap.text.toString()
+                val password = tietAddorangtuaPassword.text.toString()
+                val nomorHp = tietAddorangtuaHp.text.toString()
+                val email = tietAddorangtuaEmail.text.toString()
+                val nik = tietAddorangtuaNisn.text.toString()
+
+                val account = Account(
+                    null,
+                    password,
+                    "OrangTua",
+                    alamat,
+                    password,
+                    nomorHp,
+                    namaLengkap,
+                    email,
+                    nik,
+                    null
+                )
+
+                accountViewModel.addTeacher(session.token.toString(), account)
+                    .observe(viewLifecycleOwner) {
+                        if (it.status == "Success") {
+                            siswaViewModel.addSiswa(session.token.toString(), siswa!!)
+                                .observe(viewLifecycleOwner) {
+                                    if (it.status == "Success") {
+                                        requireContext().showToast("Berhasil registrasi Siswa & Orang Tua")
+                                        mainNavController.popBackStack(R.id.addSiswaFragment, true)
+                                    } else {
+                                        requireContext().showToast(it.message.toString())
+                                    }
+                                }
+                        } else {
+                            requireContext().showToast(it.message.toString())
+                        }
+                    }
             }
         }
     }
